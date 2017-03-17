aca370ae7c40fbe94d8ef60ee8b35719228213fc
dmitrynovik <cryinstone@gmail.com>
2017-03-17 13:52:15 +1100

    Service added

---
+++ Iasset.Service/WeatherService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using Iasset.Service.WeatherProxy;
+
+namespace Iasset.Service
+{
+    public class WeatherService
+    {
+        private readonly GlobalWeatherSoapClient _client;
+
+        public WeatherService(GlobalWeatherSoapClient client)
+        {
+            if (client == null) throw new ArgumentNullException(nameof(client));
+            _client = client;
+        }
+
+        public async Task<string> GetCitiesByCountry(string country)
+        {
+            return await _client.GetCitiesByCountryAsync(country);
+        }
+    }
+}
