78e61ffab970b13ea330eb1a4f8096daa218cdb3
dmitrynovik <cryinstone@gmail.com>
2017-03-17 13:40:28 +1100

    Add project files.

---
+++ Iasset.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Iasset.Web.Startup))]
+namespace Iasset.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
